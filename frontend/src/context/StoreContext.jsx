8cd701f81be5a82164b979c5ec019b495c6d33b5
tarkeshwar raj <tarkeshwarraj@gmail.com>
2024-09-06 16:42:11 +0530

    link update

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -7,7 +7,7 @@ const StoreContext = createContext();
 
 const StoreContextProvider = ({ children }) => {
   const currency = "$";
-  const url = "http://localhost:5000/";
+  const url = "https://fashion-ecom-backend.onrender.com/";
   const [token, setToken] = useState("");
   const [fetchItems, setFetchItems] = useState([]);
   const [cartItems, setCartItems] = useState([]);
